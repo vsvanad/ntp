4ff179c842d6c57dee8938d0bce6edda9cea8ecf
vsvanad <36238284+vsvanad@users.noreply.github.com>
2018-03-14 09:04:24 -0700

    Update params.pp

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,9 +5,9 @@ class ntp::params {
   $config_file_mode = '0664'
   $servers = ['0.centos.pool.ntp.org', '1.centos.pool.ntp.org']
   $service_ensure = 'running'
-  $service_enable = 'true'
-  $servive_hasstatus = 'true'
-  $service_hasrestart = 'true'
+  $service_enable = true
+  $service_hasstatus = true
+  $service_hasrestart = true
   $service_name = $facts['os']['family'] ? {
     'Debian' => 'ntp',
     default  => 'ntpd',
